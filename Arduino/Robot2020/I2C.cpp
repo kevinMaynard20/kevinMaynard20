517e3437dbd3b28bd48236fdc8776fb99a41481c
Andrew S <yipten@outlook.com>
2020-01-18 21:04:48 -0500

    improve sending of large values over I2C

--- Arduino/Robot2020/I2C.cpp
+++ Arduino/Robot2020/I2C.cpp
@@ -24,34 +24,49 @@ static void I2C::requestEvent() {
 
 static void I2C::setWriteData(bool targetInView, int xValue, int distance) {
   writeData[0] = targetInView ? 1 : 0;
+  splitValue(xValue, 1, 3);
+  splitValue(distance, 4, 6);
 
-  if (xValue <= 127) {
-    writeData[1] = xValue;
-    writeData[2] = 0;
-    writeData[3] = 0;
-  } else {
-    writeData[1] = 127;
-    if (xValue <= 255) {
-      writeData[2] = xValue - 127;
-      writeData[3] = 0;
-    } else {
-      writeData[2] = 127;
-      writeData[3] = xValue - 254;
-    }
-  }
+//  if (xValue <= 127) {
+//    writeData[1] = xValue;
+//    writeData[2] = 0;
+//    writeData[3] = 0;
+//  } else {
+//    writeData[1] = 127;
+//    if (xValue <= 255) {
+//      writeData[2] = xValue - 127;
+//      writeData[3] = 0;
+//    } else {
+//      writeData[2] = 127;
+//      writeData[3] = xValue - 254;
+//    }
+//  }
+//
+//  if (distance <= 127) {
+//    writeData[4] = distance;
+//    writeData[5] = 0;
+//    writeData[6] = 0;
+//  } else {
+//    writeData[4] = 127;
+//    if (distance <= 255) {
+//      writeData[5] = distance - 127;
+//      writeData[6] = 0;
+//    } else {
+//      writeData[5] = 127;
+//      writeData[6] = distance - 254;
+//    }
+//  }
+}
 
-  if (distance <= 127) {
-    writeData[4] = distance;
-    writeData[5] = 0;
-    writeData[6] = 0;
-  } else {
-    writeData[4] = 127;
-    if (distance <= 255) {
-      writeData[5] = distance - 127;
-      writeData[6] = 0;
+static void I2C::splitValue(int value, byte startIndex, byte endIndex) {
+  for (byte i = startIndex; i <= endIndex; i++) {
+    if (value <= 127) {
+      writeData[i] = value;
+      for (byte j = i + 1; j <= endIndex; j++)
+        writeData[j] = 0;
     } else {
-      writeData[5] = 127;
-      writeData[6] = distance - 254;
+      writeData[i] = 127;
+      value -= 127;
     }
   }
 }
